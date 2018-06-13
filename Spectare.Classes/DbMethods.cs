a4bab78b1e8fc46e21399cf64a1113f2c7513968
TatyanaKnyazeva <ktvshka99@gmail.com>
2018-06-13 11:57:51 +0300

    Добавлены новые ментоды в DbMethods

--- Spectare.Classes/DbMethods.cs
+++ Spectare.Classes/DbMethods.cs
@@ -9,6 +9,8 @@ namespace Spectare.Classes
 {
     public class DbMethods
     {
+        User _user;
+
         public static string GetHash(string password)
         {
             var md5 = MD5.Create();
@@ -28,6 +30,39 @@ namespace Spectare.Classes
             using (Context context = new Context())
             {
                 context.Users.Add(NewUser);
+                context.SaveChanges();
+            }
+        }
+
+        public bool Authorization(string email, string password)
+        {
+            using (var context = new Context())
+            {
+                password = GetHash(password);
+                var user = context.Users.Include("FavFilms").FirstOrDefault(u => u.Email == email && u.Password == password);
+                if (user != null)
+                {
+                    _user = user;
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public void AddToFavorites(User user, Film film)
+        {
+            using (var context = new Context())
+            {
+                context.Users.Include("FavFilms").FirstOrDefault(u => u.Name == user.Name).FavFilms.Add(film);
+                context.SaveChanges();
+            }
+        }
+
+        public void RemoveFromFavorites(User user, Film film)
+        {
+            using (var context = new Context())
+            {
+                context.Users.Include("FavFilms").FirstOrDefault(u => u.Name == user.Name).FavFilms.Remove(film);
                 context.SaveChanges();
             }
         }
